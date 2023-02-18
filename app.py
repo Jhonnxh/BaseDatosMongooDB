f620b83caeb579950ec10caeed0626176a02800b
Jhonnxh <jhonny.hernandez1807@gmail.com>
2023-02-17 21:05:32 -0600

    Codigo fuente

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from Classes import Estudiante
 from dotenv import load_dotenv
 
 def main():
-    estudiante = Estudiante(3,"carlos","martinez","32522021")
+    estudiante = Estudiante(3,"Jhonny","Lara","32522021")
     estudiante.save()
     print("Estudiante guardado")
-    estudiante.nombre="Dany"
-    estudiante.update(2)
+    estudiante.apellido="Hernandez"
+    estudiante.update(3)
     print("Estudiante actualizado")
     #Si se va a guardar un dato elimine el metodo "update"
     #Si se va a actualizar un dato elimine el metodo "save" y especifique el id del objeto a actualizar
